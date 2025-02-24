7cd7f84cffdd5e37d170d140edc0378a6f5b44a8
mayurvaishnav <mayurvaishnav366@gmail.com>
2025-02-24 10:40:43 +0000

    remoe auto refresh

--- resources/views/layouts/adminlte-app.blade.php
+++ resources/views/layouts/adminlte-app.blade.php
@@ -83,17 +83,17 @@
             });
 
             // Auto reload after 5 minutes of inactivity
-            let inactivityTime = 5 * 60 * 1000; // 5 minutes
-            let timeout;
-            function resetTimer() {
-                clearTimeout(timeout);
-                timeout = setTimeout(() => {
-                    location.reload();
-                }, inactivityTime);
-            }
-            // Detect user activity (mouse move, key press, scroll, click)
-            $(document).on('mousemove keydown scroll click', resetTimer);
-            resetTimer();
+            // let inactivityTime = 5 * 60 * 1000; // 5 minutes
+            // let timeout;
+            // function resetTimer() {
+            //     clearTimeout(timeout);
+            //     timeout = setTimeout(() => {
+            //         location.reload();
+            //     }, inactivityTime);
+            // }
+            // // Detect user activity (mouse move, key press, scroll, click)
+            // $(document).on('mousemove keydown scroll click', resetTimer);
+            // resetTimer();
 
             // DataTables
             $('.datatable').DataTable({
